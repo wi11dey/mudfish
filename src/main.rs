f5206d0ba20903f4ed32ebfaf448b301c5ac324c
Will Dey <will_dey@alumni.harvard.edu>
2024-07-20 03:33:50 -0400

    serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use rmp_serde;
+use serde::Deserialize;
 use std::path::PathBuf;
 use std::option::Option;
 use std::fs::{
@@ -12,8 +14,86 @@ use std::io::{
 use clap::Parser;
 use adblock::{
     Engine,
-    lists::{FilterSet, ParseOptions}
+    lists::{FilterSet, ParseOptions},
 };
+use std::collections::{
+    HashMap,
+    HashSet,
+};
+use std::vec::Vec;
+
+type Hash = u64;
+type NetworkFilterMask = u32;
+
+#[derive(Deserialize)]
+enum FilterPart {
+    Empty,
+    // Simple(String),
+    Simple(()),
+    // AnyOf(Vec<String>),
+    AnyOf(()),
+}
+
+#[derive(Deserialize)]
+struct NetworkFilterV0DeserializeFmt {
+    _mask: NetworkFilterMask,
+    _filter: FilterPart,
+    _opt_domains: Option<Vec<Hash>>,
+    _opt_not_domains: Option<Vec<Hash>>,
+    _redirect: Option<String>,
+    _hostname: Option<String>,
+    _csp: Option<String>,
+    _bug: Option<u32>,
+    _tag: Option<String>,
+    _raw_line: Option<String>,
+    _id: Hash,
+    _opt_domains_union: Option<Hash>,
+    _opt_not_domains_union: Option<Hash>,
+}
+
+#[derive(Deserialize)]
+struct NetworkFilterListV0DeserializeFmt {
+    _filter_map: HashMap<Hash, Vec<NetworkFilterV0DeserializeFmt>>,
+}
+
+#[derive(Deserialize)]
+struct LegacyRedirectResource {
+    _content_type: String,
+    _data: String,
+}
+
+#[derive(Deserialize)]
+struct LegacyRedirectResourceStorage {
+    _resources: HashMap<String, LegacyRedirectResource>,
+}
+
+#[derive(Deserialize)]
+struct DeserializeFormat {
+    _csp: NetworkFilterListV0DeserializeFmt,
+    _exceptions: NetworkFilterListV0DeserializeFmt,
+    _importants: NetworkFilterListV0DeserializeFmt,
+    _redirects: NetworkFilterListV0DeserializeFmt,
+    _filters_tagged: NetworkFilterListV0DeserializeFmt,
+    _filters: NetworkFilterListV0DeserializeFmt,
+    _generic_hide: NetworkFilterListV0DeserializeFmt,
+
+    _tagged_filters_all: Vec<NetworkFilterV0DeserializeFmt>,
+
+    _enable_optimizations: bool,
+
+    _resources: LegacyRedirectResourceStorage,
+
+    simple_class_rules: HashSet<String>,
+    simple_id_rules: HashSet<String>,
+    complex_class_rules: HashMap<String, Vec<String>>,
+    complex_id_rules: HashMap<String, Vec<String>>,
+
+    // specific_rules: LegacyHostnameRuleDb,
+
+    // misc_generic_selectors: HashSet<String>,
+
+    // _scriptlets: LegacyScriptletResourceStorage,
+}
 
 #[derive(Parser)]
 struct Args {
@@ -40,11 +120,17 @@ fn load_filters(filters: PathBuf) -> FilterSet {
     return filter_set;
 }
 
+fn engine_internals(engine: &Engine) -> DeserializeFormat {
+    let serialized = engine.serialize_raw().expect("");
+    return rmp_serde::from_read(&serialized[5..]).expect("");
+}
+
 fn main() {
     let args = Args::parse();
 
     if let Some(filters) = args.filters {
-	Engine::from_filter_set(load_filters(filters), true);
+	let engine = Engine::from_filter_set(load_filters(filters), true);
+	engine_internals(&engine);
     }
 
     println!("Hello, world!");
